9d38cf4f2584ff199dc66e9b39d867601fc35063
magusin <fanatsy4@gmail.com>
2021-11-11 19:30:35 +0100

    home_view + button Signin Signup add

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,23 +1,61 @@
-
-
 import 'package:flutter/material.dart';
 
+import './auth/signin_view.dart';
+import './auth/signup_view.dart';
+
 class HomeView extends StatelessWidget {
-  const HomeView ({ Key? key }) : super(key: key);
+  static String routeName = '/home';
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.all(20),
         alignment: Alignment.center,
-        child: const Text(
-          'HomeView !', 
-        style: TextStyle(
-          fontSize: 35,
-          fontWeight: FontWeight.bold,
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Bienvenue sur "Recherche du nom en cours" !',
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+               style: TextButton.styleFrom(
+              backgroundColor: Colors.grey.shade900,
+              ),
+              onPressed: () {
+                 Navigator.pushNamed(context, SignupView.routeName);
+              },
+              child: const Text('S\'inscrire',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )),
+            ),
+            TextButton(
+               style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).primaryColor,
+              ),
+              onPressed: () {
+                 Navigator.pushNamed(context, SigninView.routeName);
+              },
+              child: const Text('Connexion',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+              )),
+            ),
+          ],
         ),
-        ),
-        ),
+      ),
     );
   }
 }
