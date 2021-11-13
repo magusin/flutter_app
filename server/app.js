d8b7466f3880fc18e9a2a35b57a591d06d3112d7
magusin <fanatsy4@gmail.com>
2021-11-13 22:18:51 +0100

    retour main

--- server/app.js
+++ server/app.js
@@ -65,6 +65,6 @@ app.use((err, req, res, next) => {
 // var corsOptions = {
 //  origin: *,
 //  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+//}
 
 module.exports = app;
